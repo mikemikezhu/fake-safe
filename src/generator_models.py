ed44c5ece43cc420bb25ccd2a555cbb4f4af2638
Mike Zhu <12051594@life.hkbu.edu.hk>
2020-07-04 22:42:16 +0800

    Create generic class of generator model creator
    [FakeSafe]

--- src/generator_models.py
+++ src/generator_models.py
@@ -12,6 +12,58 @@ print('Tensorflow version: {}'.format(tf.__version__))
 Generator Model Creator
 """
 
+
+class GeneratorModelCreator(AbstractModelCreator):
+
+    def __init__(self, input_shape, output_shape,
+                 from_image=False, to_image=False,
+                 activation='linear'):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.from_image = from_image
+        self.to_image = to_image
+        self.activation = activation
+
+    def create_model(self):
+
+        model = Sequential()
+
+        if self.from_image:
+            model.add(Flatten(input_shape=self.input_shape))
+        else:
+            model.add(Dense(64, input_shape=self.input_shape))
+            model.add(LeakyReLU(alpha=0.2))
+            model.add(BatchNormalization(momentum=0.8))
+            model.add(Dropout(0.2))
+
+        model.add(Dense(256))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+
+        if self.to_image:
+            model.add(Dense(np.prod(self.output_shape),
+                            activation=self.activation))
+            model.add(Reshape(self.output_shape))
+        else:
+            model.add(Dense(self.output_shape,
+                            activation=self.activation))
+
+        print('Generator model:')
+        model.summary()
+
+        return model
+
+
 """ Word / Text State -> Image """
 
 
